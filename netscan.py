93028b96905ea645a1e01af668c34bee47e7cbe2
DanielAustinLee <danielaustinlee@gmail.com>
2017-11-20 16:01:21 -0500

    Add method stubs

---
+++ netscan.py
@@ -0,0 +1,23 @@
+
+# parameter ipAddress: IP address to scan
+# returns True if host is up, False if host is down
+def pingScan(ipAddress):
+    pass
+
+#returns network address
+def getNetworkAddress():
+    pass
+
+#returns subnet mask
+def getSubnetMask():
+    pass
+
+#returns list of possible IP addresses on subnet
+def getSubnetAddresses():
+    pass
+
+#parameter addressList: list of IP addresses to scan
+def scanAddresses(addressList):
+    pass
+
+
