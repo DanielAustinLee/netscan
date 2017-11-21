f59b4e67d206a22110e113f7189a1550f4907fcb
DanielAustinLee <danielaustinlee@gmail.com>
2017-11-20 20:49:17 -0500

    Implement pingScan() and scanAddresses()

--- netscan.py
+++ netscan.py
@@ -1,23 +1,45 @@
+from netifaces import *
+import socket
+import subprocess
+import os
+import threading
+from scapy.all import *
 
 # parameter ipAddress: IP address to scan
 # returns True if host is up, False if host is down
 def pingScan(ipAddress):
-    pass
 
-#returns network address
-def getNetworkAddress():
-    pass
+    try:
+        subprocess.check_output("ping -c 1 " + ipAddress)
 
-#returns subnet mask
-def getSubnetMask():
-    pass
+    except Exception, e:
+        return False
 
-#returns list of possible IP addresses on subnet
-def getSubnetAddresses():
-    pass
+    return True
 
 #parameter addressList: list of IP addresses to scan
-def scanAddresses(addressList):
+def scanAddresses(startAddress, endAddress):
+    startAddress = startAddress.split(".")
+    endAddress = endAddress.split(".")
+
+    activeHosts = []
+
+    for firstField in range(int(startAddress[0]), int(endAddress[0]) + 1):
+
+        for secondField in range(int(startAddress[1]), int(endAddress[1]) + 1):
+
+            for thirdField in range(int(startAddress[2]), int(endAddress[2]) + 1):
+
+                for fourthField in range(int(startAddress[3]), int(endAddress[3]) + 1):
+
+                    if pingScan(str(firstField) + "." + str(secondField) + "." + str(thirdField) + "." + str(fourthField)):
+                        activeHosts.append(str(firstField) + "." + str(secondField) + "." + str(thirdField) + "." + str(fourthField))
+
+def portScan(ipAddress, port):
     pass
 
+def main():
+    print(pingScan("8.8.8.8"))
+    scanAddresses("8.8.8.8", "8.8.8.255")
 
+main()
